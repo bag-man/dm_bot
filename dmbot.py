9cd4457e9b67c8135fb37c476e4942bedbe510fa
Owen Garland <garland.owen@gmail.com>
2015-04-15 18:49:45 +0000

    Changed image host, and made it a lot more efficient.

--- dmbot.py
+++ dmbot.py
@@ -1,103 +1,96 @@
 import urllib
 import praw
-import pyimgur
-import string
-import re
+import os
+import requests
+import time
 
 from selenium import webdriver
 from PIL import Image
 from xvfbwrapper import Xvfb
 
+
 def getScreenShot(url):
-  # http://stackoverflow.com/a/6300672
-  # http://stackoverflow.com/a/15870708
+    # http://stackoverflow.com/a/6300672
+    # http://stackoverflow.com/a/15870708
 
-  vdisplay = Xvfb()
-  vdisplay.start()
+    fox.get(url)
 
-  fox = webdriver.Firefox()
-  fox.get(url)
+    element = fox.find_element_by_id('js-article-text')
+    comment = element.find_element_by_id('socialLinks')
 
-  element = fox.find_element_by_id('js-article-text')
-  location = element.location
-  size = element.size
-  fox.save_screenshot('screenshot.png') 
-  fox.quit()
+    end = comment.location
+    location = element.location
+    size = element.size
 
-  im = Image.open('screenshot.png') 
+    fox.save_screenshot('screenshot.jpg')
 
-  left = location['x']
-  top = location['y']
-  right = location['x'] + size['width']
-  bottom = location['y'] + size['height']
+    im = Image.open('screenshot.jpg')
 
-  im = im.crop((left, top, right, bottom))
-  im.save('screenshot.png')
+    left = location['x']
+    top = location['y']
+    right = location['x'] + size['width']
+    bottom = location['y'] + end['y'] - 510
 
-  vdisplay.stop()
-
-longComment =\
-"""
-Please don't post Daily Mail links!
-
-The DM is a pretty evil organisation, these videos explain it better than me:
-
-* [Why The Daily Mail is Evil](https://www.youtube.com/watch?v=r9dqNTTdYKY)
-* [The Daily Mail song](https://www.youtube.com/watch?v=5eBT6OSr1TI)
-
-You can view an image of the article [here](%s) instead of visiting their page.
-
-Or you can find another news source [here](https://www.google.co.uk/#q=%s+-site:dailymail.co.uk).
+    im = im.crop((left, top, right, bottom))
+    im.save('screenshot.jpg', "JPEG")
 
 
-*****
-^(If you want to stop this bot posting in your sub-reddit, please message /u/Midasx. Code on) ^[github](https://github.com/bag-man/dm_bot). 
+vdisplay = Xvfb()
+vdisplay.start()
 
-//	This comment is no longer used, people didn't like it. 
-//	googleUrl = re.sub('[%s]' % re.escape(string.punctuation), '', submission.title)
-//	googleUrl = googleUrl.replace(" ","+")
-"""
+fox = webdriver.Firefox()
 
-comment =\
-"""
+comment = """
 [Non-Daily Mail Mirror](%s)
 
-^(Created by /u/Midasx. Code on) ^[github](https://github.com/bag-man/dm_bot). 
+^(Created by /u/Midasx. Code on) ^[github](https://github.com/bag-man/dm_bot).
 """
 
-
-clientId = "2c85eab2ef18353"
 postedOn = []
-i = pyimgur.Imgur(clientId)
-r = praw.Reddit(user_agent='DM_Bot')
-r.login('DailMail_Bot', 'NotMyPassword')
+r = praw.Reddit(user_agent='DM_Mirror')
+
+r.login('DailMail_Bot', 'THISISDEFINITELYNOTMYPASSWORD')
+
 reddits = {'politic', 'dailymail'}
 
 print "Logged in"
 first = True
 
 while True:
-  submissions = r.get_domain_listing('dailymail.co.uk', sort='new',limit=100)
-  for submission in submissions:
-    if str(submission.subreddit).lower() not in reddits:
+    submissions = r.get_domain_listing('dailymail.co.uk', sort='new', limit=50)
+    for submission in submissions:
+        if str(submission.subreddit).lower() not in reddits:
+            if first is True:
+                postedOn.append(submission.id)
 
-      if first == True:
-	postedOn.append(submission.id)
+            if submission.id not in postedOn:
+                print "We got one! " + submission.short_link
+                if submission.domain == "dailymail.co.uk":
+                    try:
+                        getScreenShot(submission.url.rstrip())
+                    except Exception, e:
+                        print "Failed to get image:"
+                        print e
+                elif submission.domain == "i.dailymail.co.uk":
+                    urllib.urlretrieve(submission.url, "screenshot.jpg")
 
-      if submission.id not in postedOn:
-	print "We got one! " + submission.short_link
+                try:
+                    response = requests.post(
+                        url="http://pomf.se/upload.php",
+                        files={"files[]": open("screenshot.jpg", "rb")}
+                    )
+                    link = "http://a.pomf.se/" + response.text.split('"')[17]
+                    submission.add_comment(comment % (link))
+                    print "Posted!"
+                except Exception, e:
+                    print "Failed to submit:"
+                    print e
 
-	if submission.domain == "dailymail.co.uk":
-	  getScreenShot(submission.url.rstrip())
-        elif submission.domain == "i.dailymail.co.uk":
-	 urllib.urlretrieve(submission.url, "screenshot.png") 
+                if(os.path.isfile('screenshot.jpg')):
+                    os.remove('screenshot.jpg')
+                postedOn.append(submission.id)
+    time.sleep(5)
+    first = False
 
-        try:
-	  image = i.upload_image("screenshot.png")
-	  submission.add_comment(comment % (image.link))
-	  print "Posted!"
-	except:
-	  print "Failed to submit."
-
-	postedOn.append(submission.id)
-  first = False
+fox.quit()
+vdisplay.stop()
