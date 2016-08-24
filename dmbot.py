5124132d8fb4aad7592e0a907814640342e2ee77
Matt Rudge <mrudge@gmail.com>
2016-08-24 21:22:26 +0100

    Error trapping
    Added conditional checking in case Google doesn't return any results. Now just the screenshot will be returned instead of the exception.

--- dmbot.py
+++ dmbot.py
@@ -15,8 +15,11 @@ import itertools, sys
 def findAnother(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
     res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-
-    return res['items']
+    if int(res['searchInformation']['totalResults']) == 0:
+        resultsvar = []
+    else:
+        resultsvar = res['items']
+    return resultsvar
 
 def getScreenShot(url):
     # http://stackoverflow.com/a/15870708
